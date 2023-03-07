2bd802dafe1726f1022accf1d90fa9af2e76ef73
Huy  Phạm Gia <phamhuy15052004@gmail.com>
2023-03-07 11:39:05 +0700

    update

--- src/components/Exercise_Shoe.js
+++ src/components/Exercise_Shoe.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import ListShoe from './ListShoe'
 import CartShort from './CartShoe'
+import { useSelector } from 'react-redux'
 
 export default function Exercise_Shoe() {
+  const dataListShoe = useSelector(state => state.ShoeReducer.listCartShoe)
+  
 
   return (
    <div className='bg-gray-800 w-full h-full  '>
       <div className='container mx-auto h-full'>
           <h1 className='text-center py-5 text-3xl font-bold text-white'>SHOP SHOE WITH REACT FUNCTIONAL</h1>
-          <CartShort/>
+          {dataListShoe.length > 0 && <CartShort/>}
           <ListShoe /> 
       </div>
    </div>
